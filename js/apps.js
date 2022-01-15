6a4a534c1b87a6fe09928a2d79dda6bbbd3d51ab
Borhan Uddin <borhaninfos@gmail.com>
2022-01-15 21:05:49 +0600

    table add

--- js/apps.js
+++ js/apps.js
@@ -1,14 +1,81 @@
 // const userName = prompt("Type Your Name : ");
 const userName = "Borhan Uddin";
 let showName = `হ্যালো ${userName}`;
+
+var today = new Date();
+var date =
+  today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+
+//Income Function
+function income() {
+  let incDetails = document.getElementById("income-detail").value;
+  let incAmount = document.getElementById("incamount").value;
+
+  if (incAmount == "" || incAmount == NaN) {
+    document.getElementById("modal-title").innerHTML = showName;
+    document.getElementById("incMessage").innerHTML =
+      "আপনি আপনার কোন প্রকার আয় এর তথ্য প্রদান করেননি।";
+    return false;
+  } else {
+    var incNumber = parseFloat(incAmount);
+  }
+
+  if (incNumber < 0) {
+    alert("Please enter correct value greater than 0");
+  } else {
+    // Creating and adding data to table
+    let tr = document.createElement("tr");
+    // Create Table <td>
+    let crdate = document.createElement("th");
+    crdate.innerHTML = date;
+    let cedesc = document.createElement("td");
+    cedesc.innerHTML = incDetails;
+    let cramm = document.createElement("td");
+    cramm.innerHTML = incAmount;
+    // <td> Append Inside <tr>
+    tr.appendChild(crdate);
+    tr.appendChild(cedesc);
+    tr.appendChild(cramm);
+    //  Pick Income List Id
+    let tbody = document.getElementById("income-list");
+    // <tr> Append Inside Income List Id
+    tbody.appendChild(tr);
+
+    let balance = document.getElementById("prebalance").innerText;
+    let balanceAmount = parseFloat(balance);
+    let totalIncome = balanceAmount + incNumber;
+    document.getElementById("prebalance").innerText =
+      parseFloat(totalIncome).toFixed(2);
+
+    let balancenow = document.getElementById("balance").innerText;
+    let balanceNownum = parseFloat(balancenow);
+
+    let dueBalance = balanceNownum + incNumber;
+    let dueB = parseFloat(dueBalance).toFixed(2);
+
+    //Modal Show
+    document.getElementById("modal-title").innerHTML = showName;
+    document.getElementById("incMessage").innerHTML =
+      "আপনার " +
+      incDetails +
+      " আয় সর্বমোট " +
+      incNumber +
+      " টাকা সফল ভাবে যোগ হয়েছে। ";
+
+    document.getElementById("balance").innerHTML = dueB;
+    document.getElementById("income-detail").value = "";
+    document.getElementById("incamount").value = "";
+  }
+}
+
 //Expense Function
 function expense() {
   var exDetails = document.getElementById("ex-detail").value;
   var exAmount = document.getElementById("ex-amount").value;
   if (exAmount == "" || exAmount == NaN) {
-    document.getElementById("modal-titlex").innerHTML = showName;
-    document.getElementById("modalTitleex").innerHTML =
-      "আপনি কোন সংখা প্রদান করেননি।";
+    document.getElementById("exp-title").innerHTML = showName;
+    document.getElementById("expMessage").innerHTML =
+      "আপনি আপনার কোন প্রকার ব্যয় এর তথ্য প্রদান করেননি।";
     return false;
   } else {
     var exAmountnumber = parseFloat(exAmount);
@@ -17,12 +84,30 @@ function expense() {
   if (exAmountnumber < 0) {
     alert("Please Input Correct Value greater than 0");
   } else {
-    var exlist = document.createTextNode(
-      exDetails + " = " + exAmount + " Taka"
-    );
-    var list = document.createElement("li");
-    list.appendChild(exlist);
-    document.getElementById("expense-list").appendChild(list);
+    // Creating and adding data to table
+    let tr = document.createElement("tr");
+    // Create Table <td>
+    let crdate = document.createElement("th");
+    crdate.innerHTML = date;
+    let cedesc = document.createElement("td");
+    cedesc.innerHTML = exDetails;
+    let cramm = document.createElement("td");
+    cramm.innerHTML = exAmount;
+    // <td> Append Inside <tr>
+    tr.appendChild(crdate);
+    tr.appendChild(cedesc);
+    tr.appendChild(cramm);
+    //  Pick Income List Id
+    let tbody = document.getElementById("expense-list");
+    // <tr> Append Inside Income List Id
+    tbody.appendChild(tr);
+
+    // var exlist = document.createTextNode(
+    //   exDetails + " = " + exAmount + " Taka"
+    // );
+    // var list = document.createElement("li");
+    // list.appendChild(exlist);
+    // document.getElementById("expense-list").appendChild(list);
 
     var expenseBalance = document.getElementById("total-expense").innerText;
     var expenseNumber = parseFloat(expenseBalance);
@@ -38,57 +123,16 @@ function expense() {
     document.getElementById("total-expense").innerHTML =
       parseFloat(totalExpense).toFixed(2);
 
-    document.getElementById("modalTitleex").innerHTML =
-      "আপনার " + exDetails + " খরচ টি সফল ভাবে যোগ হয়েছে। ";
-    document.getElementById("modalpriceex").innerHTML =
-      "সর্বমোট " + exAmountnumber + " টাকা। ";
+    //Modal Show
+    document.getElementById("exp-title").innerHTML = showName;
+    document.getElementById("expMessage").innerHTML =
+      "আপনার " +
+      exDetails +
+      " ব্যয় সর্বমোট " +
+      exAmountnumber +
+      " টাকা সফল ভাবে যোগ হয়েছে। ";
 
     document.getElementById("ex-detail").value = "";
     document.getElementById("ex-amount").value = "";
-  }
-}
-
-//Income Function
-function income() {
-  let incDetails = document.getElementById("income-detail").value;
-  let incAmount = document.getElementById("incamount").value;
-
-  if (incAmount == "" || incAmount == NaN) {
-    document.getElementById("modal-title").innerHTML = showName;
-    document.getElementById("modalTitle").innerHTML =
-      "আপনি কোন সংখা প্রদান করেননি।";
-    return false;
-  } else {
-    var incNumber = parseFloat(incAmount);
-  }
-
-  if (incNumber < 0) {
-    alert("Please enter correct value greater than 0");
-  } else {
-    let incList = document.createTextNode(
-      incDetails + " = " + incAmount + " Taka"
-    );
-    let inli = document.createElement("li");
-    inli.appendChild(incList);
-    document.getElementById("income-list").appendChild(inli);
-    let balance = document.getElementById("prebalance").innerText;
-    let balanceAmount = parseFloat(balance);
-    let totalIncome = balanceAmount + incNumber;
-    document.getElementById("prebalance").innerText =
-      parseFloat(totalIncome).toFixed(2);
-
-    let balancenow = document.getElementById("balance").innerText;
-    let balanceNownum = parseFloat(balancenow);
-
-    let dueBalance = balanceNownum + incNumber;
-    let dueB = parseFloat(dueBalance).toFixed(2);
-    document.getElementById("modalTitle").innerHTML =
-      "আপনার " + incDetails + " ইনকাম টি সফল ভাবে যোগ হয়েছে। ";
-    document.getElementById("modalprice").innerHTML =
-      "সর্বমোট " + incNumber + " টাকা। ";
-
-    document.getElementById("balance").innerHTML = dueB;
-    document.getElementById("income-detail").value = "";
-    document.getElementById("incamount").value = "";
   }
 }
